1fd79ecc015f7d38ade2d70e62c0798999a0f7f8
llblilibang <12609851+llblilibang@user.noreply.gitee.com>
2023-04-07 14:06:01 +0800

    初始化

---
+++ spring-boot-miaosha/src/main/java/com/itstyle/seckill/common/webSocket/WebSocketConfig.java
@@ -0,0 +1,15 @@
+package com.itstyle.seckill.common.webSocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+/**
+ * WebSocket配置
+ */
+@Configuration  
+public class WebSocketConfig {  
+    @Bean  
+    public ServerEndpointExporter serverEndpointExporter() {  
+        return new ServerEndpointExporter();  
+    }  
+}  
---
+++ spring-boot-miaosha/src/main/java/com/itstyle/seckill/service/ICreateHtmlService.java
@@ -0,0 +1,10 @@
+package com.itstyle.seckill.service;
+
+import com.itstyle.seckill.common.entity.Result;
+
+/**
+ * 生成商品静态页
+ */
+public interface ICreateHtmlService {
+	Result createAllHtml();
+}
